3ec0271c4ce2add7b02c845b5297a86f4ffee7cd
Documenter.jl <documenter@juliadocs.github.io>
2026-08-28 13:56:42 +0000

    build based on ac700c70

---
+++ v0.7/assets/chunks/channel.Bo9-W8hD.js
@@ -0,0 +1,1 @@
+import{U as a,C as n}from"../app.DDsED2Ga.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
---
+++ v0.7/assets/chunks/huet_sayegh_2s2p1d.BsiPqSIE.js
@@ -0,0 +1,1 @@
+const s="/MeanFieldHomogenization.jl/v0.7/assets/huet_sayegh_2s2p1d.XF_GnAH1.svg";export{s as _};
