2743f567f5b2a929f549f92dda0e7de9919aee1b
Documenter.jl <documenter@juliadocs.github.io>
2026-09-07 17:23:55 +0000

    build based on fa913243

--- dev/assets/chunks/channel.DyL2L37l.js
+++
@@ -1,1 +0,0 @@
-import{U as a,C as n}from"../app.B59JPybA.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
---
+++ dev/assets/chunks/swimlanesDiagram-VR7AAH4N.BA-ZmMVD.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW.D_1dDqs3.js";import{_ as a}from"../app.ByCHe-HC.js";import"./chunk-5VM5RSS4.DS7nhH1r.js";import"./chunk-XXDRQBXY.DR_vuvUk.js";import"./chunk-POPQ4Y6H.DDmYf83q.js";import"./chunk-F27PBJKO.cvLOeArG.js";import"./channel.DvjUh5BR.js";import"./framework.CIBJwm-3.js";import"./commonjsHelpers.Cpj98o6Y.js";import"./theme.DvCf1d6y.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,_=r({defaultLayout:"swimlane",styles:m});export{_ as diagram};
