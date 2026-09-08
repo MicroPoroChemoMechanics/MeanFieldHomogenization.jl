bdf422e1805c19710dd7978548c786bbece1417d
Documenter.jl <documenter@juliadocs.github.io>
2026-09-08 18:41:44 +0000

    build based on 76d7bfaa

---
+++ v0.13/assets/chunks/swimlanesDiagram-VR7AAH4N.DMaKoJ8p.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW.DV7JUzjA.js";import{_ as a}from"../app.DGsbvXdX.js";import"./chunk-5VM5RSS4.CBwIm3C7.js";import"./chunk-XXDRQBXY.ysVlo7zG.js";import"./chunk-POPQ4Y6H.DeZlK962.js";import"./chunk-F27PBJKO.CdN1thOm.js";import"./channel.C6ftSZN3.js";import"./framework.BV7Zve7q.js";import"./commonjsHelpers.Cpj98o6Y.js";import"./theme.BlO3awqc.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,_=r({defaultLayout:"swimlane",styles:m});export{_ as diagram};
--- versions.js
+++ versions.js
@@ -1,6 +1,7 @@
 var DOC_VERSIONS = [
   "dev",
   "stable",
+  "v0.13",
   "v0.12",
   "v0.11",
   "v0.10",
@@ -8,5 +9,5 @@ var DOC_VERSIONS = [
   "v0.8",
   "v0.7",
 ];
-var DOCUMENTER_NEWEST = "0.12.0";
+var DOCUMENTER_NEWEST = "0.13.0";
 var DOCUMENTER_STABLE = "stable";
